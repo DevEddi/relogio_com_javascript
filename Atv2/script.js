dc4bfd33e42fa3f8b1802987f3e3c4270a787cd8
Edmilson <scovinowork@gmail.com>
2021-09-15 23:44:40 -0300

    cronometro, mas não finalizado

--- Atv2/script.js
+++ Atv2/script.js
@@ -1,16 +1,34 @@
+var tempoEscolhido = document.getElementById('tempo').value;
 
-var tempoEscolhido = document.getElementById("timeForm").submit();
-function recebeTempo(){    
-    resultado = tempoEscolhido - 1 >= 0 ? tempoEscolhido-- : "FIM";
-    document.getElementById("tempoDescrescente").time.innerText = resultado;    
-    setTimeout("recebeTempo();", 1000);
+function iniciaTempo(duracao, display){   
+  
+  var timer = duracao, tempoEscolhido;
+  
+  setInterval(function() {
+    tempoEscolhido = parseInt(timer % 60, 10);
+    tempoEscolhido = tempoEscolhido < 10 ? "0" + tempoEscolhido : tempoEscolhido;
+    display.textContent = tempoEscolhido;
+    
+    if(--timer < 0){
+      timer = tempoEscolhido;
+    }
+    
+  }, 1000); 
+       
+}
+
+window.onload = function(){
+  var duracao  = 60 * 4;
+  var display = document.querySelector("#timer"); 
+  iniciaTempo(duracao, display);
 }
 
 /*
 var number = prompt("Digite o número de segundos para a contagem regressiva");
 function countDown() {
-  result = number - 1 >= 0 ? number-- : "FIM";
-  document.getElementById("tempoDescrescente").time.innerText = result; // innerText é uma propriedade que representa o conteúdo textual "renderizado" de um nó e seus descendentes.
-  setTimeout("countDown();", 1000); //Executa um bloco específico uma vez depois de um determinado tempo.
+  var result = number -1 >= 0 ? number-- : "FIM";
+  document.getElementById("tempoDescrescente").time.innerHTML = result; // innerText é uma propriedade que representa o conteúdo textual "renderizado" de um nó e seus descendentes.
+   //Executa um bloco específico uma vez depois de um determinado tempo.
 }
+setTimeout("countDown();", 1000);
 */
